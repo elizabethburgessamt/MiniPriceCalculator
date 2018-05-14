179dffd1b7519c5047f58ee2cc7b9babf50280af
Silver Dragon <silverdragon013@hotmail.com>
2018-05-14 10:37:19 -0400

    Created page for the Cart and added Add/Remove functionality

---
+++ MiniPriceCalculator/MiniPriceCalculator.WebUI/Controllers/CartController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MiniPriceCalculator.Domain.Abstract;
+using MiniPriceCalculator.Domain.Entities;
+using MiniPriceCalculator.WebUI.Models;
+
+namespace MiniPriceCalculator.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+
+        public CartController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToRouteResult AddToCart(int productID, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productID);
+
+            if (product != null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int productID, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productID);
+
+            if (product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+        
+    }
+}
