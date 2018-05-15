9b20fb046e7017f3dda5da2b15098b459c02d290
Silver Dragon <silverdragon013@hotmail.com>
2018-05-15 11:26:35 -0400

    Changed from GetCart method to using a Model Binder

--- MiniPriceCalculator/MiniPriceCalculator.UnitTests/CartTests.cs
+++ MiniPriceCalculator/MiniPriceCalculator.UnitTests/CartTests.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Moq;
+using System.Web.Mvc;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MiniPriceCalculator.Domain.Entities;
+using MiniPriceCalculator.Domain.Abstract;
+using MiniPriceCalculator.WebUI.Controllers;
+using MiniPriceCalculator.WebUI.Models;
 
 namespace MiniPriceCalculator.UnitTests
 {
@@ -118,6 +123,73 @@ namespace MiniPriceCalculator.UnitTests
 
             //Assert
             Assert.AreEqual(target.Lines.Count(), 0);
+        }
+
+        [TestMethod]
+        public void Can_Add_To_Cart()
+        {
+            //Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples" },
+
+            }.AsQueryable());
+
+            //Arrange - create a cart
+            Cart cart = new Cart();
+
+            //Arrange - create the controller
+            CartController target = new CartController(mock.Object);
+
+            //Act - add a product to the cart
+            target.AddToCart(cart, 1, null);
+
+            //Assert
+            Assert.AreEqual(cart.Lines.Count(), 1);
+            Assert.AreEqual(cart.Lines.ToArray()[0].Product.ProductID, 1);
+        }
+
+        [TestMethod]
+        public void Adding_Product_To_Cart_Goes_To_Cart_Screen()
+        {
+            //Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples" },
+
+            }.AsQueryable());
+
+            //Arrange - create a cart
+            Cart cart = new Cart();
+
+            //Arrange - create the controller
+            CartController target = new CartController(mock.Object);
+
+            //Act - add a product to the cart
+            RedirectToRouteResult result = target.AddToCart(cart, 1, null);
+
+            //Assert
+            Assert.AreEqual(result.RouteValues["action"], "Index");
+            Assert.AreEqual(result.RouteValues["returnUrl"], "myUrl");
+        }
+
+        [TestMethod]
+        public void Can_View_Cart_Contents()
+        {
+            //Arrange - create a cart
+            Cart cart = new Cart();
+
+            //Arrange - create the controller
+            CartController target = new CartController(null);
+
+            //Act - call the Index action method
+            CartIndexViewModel result = (CartIndexViewModel)target.Index(cart, "myUrl").ViewData.Model;
+
+            //Assert
+            Assert.AreSame(result.Cart, cart);
+            Assert.AreEqual(result.ReturnUrl, "myUrl");
         }
     }
 }
--- MiniPriceCalculator/MiniPriceCalculator.WebUI/Global.asax.cs
+++ MiniPriceCalculator/MiniPriceCalculator.WebUI/Global.asax.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using MiniPriceCalculator.Domain.Entities;
+using MiniPriceCalculator.WebUI.Infrastructure.Binders;
 
 namespace MiniPriceCalculator.WebUI
 {
@@ -13,6 +15,7 @@ namespace MiniPriceCalculator.WebUI
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
         }
     }
 }
