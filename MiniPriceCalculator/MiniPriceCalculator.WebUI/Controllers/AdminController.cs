a79b5fb9cffc1c486a3372d51e2b823307d5b82e
Silver Dragon <silverdragon013@hotmail.com>
2020-05-20 11:51:13 -0400

    Added the start of the Admin functionality

---
+++ MiniPriceCalculator/MiniPriceCalculator.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MiniPriceCalculator.Domain.Abstract;
+using MiniPriceCalculator.Domain.Entities;
+
+namespace MiniPriceCalculator.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repos)
+        {
+            repository = repos;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+
+        public ViewResult Edit(int productID)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productID);
+
+            return View(product);
+        }
+    }
+}
