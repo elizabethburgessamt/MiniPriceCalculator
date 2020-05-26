3300f79945eee66cb8cb836f652c59da700ec002
Silver Dragon <silverdragon013@hotmail.com>
2020-05-26 11:33:58 -0400

    Added image data to products

--- MiniPriceCalculator/MiniPriceCalculator.WebUI/Controllers/ProductController.cs
+++ MiniPriceCalculator/MiniPriceCalculator.WebUI/Controllers/ProductController.cs
@@ -41,5 +41,18 @@ namespace MiniPriceCalculator.WebUI.Controllers
             return View(model);
         }
 
+        public FileContentResult GetImage(int productID)
+        {
+            Product prod = repository.Products.FirstOrDefault(p => p.ProductID == productID);
+            if (prod != null)
+            {
+                return File(prod.ImageData, prod.ImageMimeType);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
     }
 }
