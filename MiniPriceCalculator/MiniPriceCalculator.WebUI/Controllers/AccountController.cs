f54e60f652a10aefbfa2753f80699d436375181e
Silver Dragon <silverdragon013@hotmail.com>
2020-06-01 11:49:32 -0400

    Added login mvc

---
+++ MiniPriceCalculator/MiniPriceCalculator.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Security.Policy;
+using System.Web.Mvc;
+using MiniPriceCalculator.WebUI.Infrastructure.Abstract;
+using MiniPriceCalculator.WebUI.Models;
+
+namespace MiniPriceCalculator.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        IAuthProvider authProvider;
+
+        public AccountController(IAuthProvider auth)
+        {
+            authProvider = auth;
+        }
+
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (authProvider.Authenticate(model.UserName, model.Password))
+                {
+                    return Redirect(returnUrl ?? Url.Action("Index", "Admin"));
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Incorrect username or password");
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
